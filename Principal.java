ba364666cac5eeb315ae74e5fa8b1473194bfe0c
ept-IesCierva <163144578+ept-IesCierva@users.noreply.github.com>
2024-03-12 23:09:24 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,5 +1,9 @@
+import java.util.Date;
 public class Principal {
 	public static void main(String args[]) {
 		System.out.println("Hola Mundo");
+	}
+	public static void time() {
+		System.out.println("Hora acutal:" + new Date());
 	}
 }
